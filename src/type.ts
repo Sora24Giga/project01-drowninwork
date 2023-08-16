09c6e4932863e49b0869a765691115ae2d0c4ae1
pipe-lines <142323910+pipe-lines@users.noreply.github.com>
2023-08-16 20:33:11 +0700

    type.ts and StudentCard.vue is added

---
+++ src/type.ts
@@ -0,0 +1,14 @@
+export interface StudentDetail{
+    sid: number
+    first_name: string
+    last_name: string
+    studentImgLink: string
+    course: string
+    advisor: string
+}
+
+export interface AdvisorDetail{
+    first_name: string
+    last_name: string
+    advisorImgLink: string
+}
