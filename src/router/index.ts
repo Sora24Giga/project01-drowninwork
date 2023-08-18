d9e2c129a1bc3f04e68c860b39adf189ab8744e4
pipe-lines <142323910+pipe-lines@users.noreply.github.com>
2023-08-19 03:01:02 +0700

    URL fixed

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,7 +7,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/students',
       name: 'studentlist',
       component: StudentListView
     },
