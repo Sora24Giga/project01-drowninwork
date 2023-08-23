5ead3c9f9daa23152e010d1ef1bcd49b93e139a9
pipe-lines <142323910+pipe-lines@users.noreply.github.com>
2023-08-24 03:09:12 +0700

    StudentDetail Edited
    Button has to be fixed in position. Comment Box is Created.

--- src/views/details/StudentDetailView.vue
+++ src/views/details/StudentDetailView.vue
@@ -19,6 +19,7 @@ StudentService.getStudentsById(Number(props.id)).then((response) => {
 <template>
   <div class="student-box">
   <div class="info-pg-bg">
+    <p class="label-card">STUDENT INFO.</p>
     <div v-if="student" class="student-card">
       <br>
       <br>
@@ -31,10 +32,9 @@ StudentService.getStudentsById(Number(props.id)).then((response) => {
         <a v-bind:href="student.studentImgLink">[Profile Photo]</a>
     </div>
     <RouterLink :to="{name:'advisorDetail', params: { id: student?.advisorID } }" class="advisor-card">Advisor: {{ student?.advisor }}</RouterLink>
-    <div class="comment-card">
-      <p>Advisor Comment</p>
-
-    </div>
+    <p class="label-card">Advisor Comment</p>
+    <textarea class="comment-card"></textarea>
+    <button class="button2">EDIT</button>
     </div>
   </div>
 </template>
@@ -53,7 +53,7 @@ StudentService.getStudentsById(Number(props.id)).then((response) => {
   height: 400px;
   cursor: pointer;
   margin-bottom: 18px;
-  margin-top: 5%;
+  margin-top: 0%;
   margin-left: 32%;
   color: aliceblue;
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
@@ -81,6 +81,23 @@ StudentService.getStudentsById(Number(props.id)).then((response) => {
   width: 500px;
   height: 200px;
   cursor: pointer;
+
+  /* border: 1px solid #39495c; */
+  margin-top: 0%;
+
+  margin-left: 32%;
+  color: aliceblue;
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+  background-color: #312f2f3a;
+}
+
+.label-card{
+  text-align: center;
+  padding: 20px;
+  width: 500px;
+  height: 60px;
+  cursor: pointer;
+
   /* border: 1px solid #39495c; */
   margin-top: 3%;
 
@@ -106,6 +123,23 @@ StudentService.getStudentsById(Number(props.id)).then((response) => {
   margin-bottom: 20px;
   position: fixed; top: 0%; right: 0%;
   background-color: rgb(41, 39, 38);
+}
+
+.button{
+  position: absolute;
+  background-color: aliceblue;
+  width: 100px;
+  top: 88%;
+  right: 40%;
+  align-items: center;
+}
+.button2{
+  position: relative;
+  background-color: rgb(225, 226, 227);
+  width: 100px;
+  top:30px;
+  right: 100px;
+  align-items: center;
 }
 
 </style>
