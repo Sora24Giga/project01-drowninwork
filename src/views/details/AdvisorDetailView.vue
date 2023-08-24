68d8b74aa2aee322b3a595a0e66ea129acd95e88
NOHP404 <poopa_somth@cmu.ac.th>
2023-08-24 17:09:49 +0700

    change AdvisorDetailView.vue to tailwind

--- src/views/details/AdvisorDetailView.vue
+++ src/views/details/AdvisorDetailView.vue
@@ -7,15 +7,23 @@ const advisor = storeToRefs(store).advisor
 </script>
 
 <template>
-  <div class="info-pg-bg">
-    <p class="label-card">ADVISOR INFO.</p>
-    <div v-if="advisor" class="advisor-card">
-      <br />
-      <br />
-      <br />
-      <br />
-      <p>Name: {{ advisor.first_name }} {{ advisor.last_name }}</p>
-      <br />
+  <div class="mt-[108px] flex justify-center lg:mt-0">
+    <div class="mb-[20px] flex h-full w-[80%] justify-center bg-se-dark p-[20px] lg:ml-[20%]">
+      <div class="flex w-[60%] min-w-[216px] max-w-[500px] flex-col justify-center">
+        <p
+          class="h-[60px] w-full self-center bg-[#312f2f3a] py-[15px] text-center font-medium text-se-light-gray shadow-[0_3px_12px_0_rgba(0,0,0,0.2)]"
+        >
+          ADVISOR INFO
+        </p>
+        <div
+          v-if="advisor"
+          class="mb-[18px] mt-0 w-full bg-[#f2f2f208] p-[20px] text-center text-se-light-gray shadow-[0_3px_12px_0_rgba(0,0,0,0.2)]"
+        >
+          <img :src="advisor.img" />
+          <p class="my-2 font-semibold">{{ advisor.first_name }} {{ advisor.last_name }}</p>
+          <br />
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- src/views/details/StudentDetailView.vue
+++ src/views/details/StudentDetailView.vue
@@ -9,37 +9,37 @@ const student = storeToRefs(store).student
 <template>
   <div class="student-box mt-[108px] flex justify-center lg:mt-0">
     <div class="mb-[20px] flex h-full w-[80%] justify-center bg-se-dark p-[20px] lg:ml-[20%]">
-      <div class="flex flex-col justify-center">
+      <div class="flex w-[60%] min-w-[216px] max-w-[500px] flex-col justify-center">
         <p
-          class="h-[60px] w-[500px] self-center bg-[#312f2f3a] py-[15px] text-center font-medium text-se-light-gray shadow-[0_3px_12px_0_rgba(0,0,0,0.2)]"
+          class="h-[60px] w-full self-center bg-[#312f2f3a] py-[15px] text-center font-medium text-se-light-gray shadow-[0_3px_12px_0_rgba(0,0,0,0.2)]"
         >
-          STUDENT INFO.
+          STUDENT INFO
         </p>
         <div
           v-if="student"
-          class="mb-[18px] mt-0 w-[500px] bg-[#f2f2f208] p-[20px] text-center text-se-light-gray shadow-[0_3px_12px_0_rgba(0,0,0,0.2)]"
+          class="mb-[18px] mt-0 w-full bg-[#f2f2f208] p-[20px] text-center text-se-light-gray shadow-[0_3px_12px_0_rgba(0,0,0,0.2)]"
         >
           <div class="mx-auto my-4 w-fit overflow-hidden rounded-[50%]">
             <img :src="student.img" class="h-44 object-contain" />
           </div>
-          <p>Name: {{ student.first_name }} {{ student.last_name }}</p>
+          <p class="mb-2 font-semibold">{{ student.first_name }} {{ student.last_name }}</p>
           <p>Student ID: {{ student.sid }}</p>
           <p>Course: {{ student.course }}</p>
           <br />
         </div>
         <RouterLink
           :to="{ name: 'advisorDetail', params: { id: student?.advisorID } }"
-          class="h-[50px] w-[500px] bg-[#f2f2f208] px-[20px] py-[10px] text-center font-medium text-se-light-gray shadow-[0_3px_12px_0_rgba(0,0,0,0.2)] transition hover:scale-[1.05] hover:text-se-color-light"
+          class="h-[50px] w-full bg-[#f2f2f208] px-[20px] py-[10px] text-center font-medium text-se-light-gray shadow-[0_3px_12px_0_rgba(0,0,0,0.2)] transition hover:scale-[1.05] hover:text-se-color-light"
           >Advisor: {{ student?.advisor }}</RouterLink
         >
         <p
-          class="h-[50px] w-[500px] bg-[#312f2f3a] px-[20px] py-[10px] text-center text-se-light-gray shadow-[0_3px_12px_0_rgba(0,0,0,0.2)]"
+          class="h-[50px] w-full bg-[#312f2f3a] px-[20px] py-[10px] text-center text-se-light-gray shadow-[0_3px_12px_0_rgba(0,0,0,0.2)]"
         >
-          Advisor Comment
+          Advisor's Comment
         </p>
         <textarea
           placeholder="Add a comment here."
-          class="mt-0 h-[200px] w-[500px] border-2 border-solid border-se-light-gray bg-[#f2f2f208] p-[20px] text-se-light-gray shadow-[0_3px_12px_0_rgba(0,0,0,0.2)] focus:animate-pulse focus:text-se-white"
+          class="mt-0 h-[200px] w-full border-2 border-solid border-se-light-gray bg-[#f2f2f208] p-[20px] text-se-light-gray shadow-[0_3px_12px_0_rgba(0,0,0,0.2)] focus:animate-pulse focus:text-se-white"
         ></textarea>
         <button
           class="mt-1 w-[70%] self-center bg-se-color text-se-light-gray transition hover:scale-[1.05] hover:bg-se-color-light hover:text-se-white"
