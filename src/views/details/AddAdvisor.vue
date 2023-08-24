9cb42b49c4a9a62b3bac5df378480cead3826950
pipe-lines <142323910+pipe-lines@users.noreply.github.com>
2023-08-24 18:52:55 +0700

    Design Complete

--- src/views/details/AddAdvisor.vue
+++ src/views/details/AddAdvisor.vue
@@ -1,3 +1,45 @@
 <template>
-    <div></div>
+    <div class="invisible-box" style="padding-left: 20px;">
+        <p style="text-align: left; color:rgb(205, 207, 209)"><br>Advisor Form</p>
+        <br>
+        <br>
+        <span style="color:rgb(193, 193, 193)">FirsName: <br><textarea style="height: 25px; width: 300px; background-color: rgb(219, 219, 219); color:black"></textarea></span>
+        <br>
+        <span style="color:rgb(193, 193, 193)">LastName: <br><textarea style="height: 25px; width: 300px; background-color: rgb(219, 219, 219); color:black"></textarea></span>
+        <br>
+        <span style="color:rgb(193, 193, 193)">Advisor ID: <br><textarea style="height: 25px; background-color: rgb(219, 219, 219); color:black"></textarea></span>
+        <br>
+        <span style="color:rgb(193, 193, 193)">User List ID: <br><textarea style="height: 25px; background-color: rgb(219, 219, 219); color:black"></textarea></span>
+        <br>
+        <span style="color:rgb(193, 193, 193)">Advisor Profile Photo PATH: <br><textarea style="height: 25px; width: 300px; background-color: rgb(219, 219, 219); color:black"></textarea></span>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <button class="button">ADD</button>
+    </div>
 </template>
+
+<style scoped>
+.invisible-box{
+  width: 400px;
+  height: 534px;
+
+  margin-top: 4%;
+  margin-left: 47%;
+  margin-bottom: 50%;
+  margin-right: 50%;
+  border: 1px solid;
+  background-color: rgba(30, 30, 30, 0.36);
+
+}
+
+.button{
+    margin-left: 80%;
+    width: 60px;
+    height: 30px;
+    background-color: rgb(154, 156, 157);
+}
+</style>
