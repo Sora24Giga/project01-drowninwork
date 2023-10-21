2e943a5f29f2eed82202e1c89c39d0521f790b82
pipe-lines <changseong_lee@cmu.ac.th>
2023-10-21 21:00:22 +0700

    Requirement View UI Design

--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -9,11 +9,13 @@ export default defineComponent({
 
 <template>
 <!--Router AdvisorFormView and StudentFromView should be added.-->
-  <nav class="box"><!--this box css is just a temp.-->
-  <router-link to="add-advisor">Advisor registration</router-link>
+  <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+  <nav><!--this box css is just a temp.-->
+  <router-link to="add-advisor" class="mt-1 w-full border-2 bg-transparent p-0 focus:border-transparent focus:outline-1 focus:ring-0 sm:text-sm text-se-white">Advisor registration</router-link>
     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-  <router-link to="add-student">Student registration</router-link>
+  <router-link to="add-student" class="mt-4 w-full border-2 bg-transparent p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm text-se-white">Student registration</router-link>
   </nav>
+  </div>
 </template>
 
 <style scoped>
