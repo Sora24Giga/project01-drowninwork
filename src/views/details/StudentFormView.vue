d7d4971bedc08adf943871663d71c0657e27d7a8
pipe-lines <changseong_lee@cmu.ac.th>
2023-10-21 20:16:56 +0700

    Student form UI Copied

--- src/views/details/StudentFormView.vue
+++ src/views/details/StudentFormView.vue
@@ -64,39 +64,107 @@ function saveStudent() {
 
 <template>
   <div class="text-xs md:text-base mb-[2rem] ml-0 mt-[108px] h-full w-full bg-se-dark lg:ml-[20%] lg:mt-[60px] lg:w-[80%] flex flex-col">
-    <h1>Create a registration</h1>
+    <header>
+      <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+          <div class="text-center sm:text-left">
+            <h1 class="text-2xl font-bold text-se-white sm:text-3xl">
+              Create a registration
+            </h1>
+            <p class="mt-1.5 text-sm text-se-white">
+              Register Student Information 🗿
+            </p>
+          </div>
+          <div class="mt-4 flex flex-col gap-4 sm:mt-0 sm:flex-row sm:items-center">
+          </div>
+        </div>
+      </div>
+    </header>
     <form @submit.prevent="saveStudent" >
-      <div>
-        <h3>Register Student Information</h3>
+      <div class="ml-16 mr-64">
+
+        <!-- Academic position -->
+        <label for="AcademicPo"
+               class="block overflow-hidden mb-8 rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600 bg-se-black1800">
+          <span class="text-xs font-medium text-se-white">
+            Academic Position
+          </span>
+
+          <BaseInput v-model="student.studentId" type="text" placeholder="Insert student ID here"
+                     class="mt-1 w-full border-none bg-transparent p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm text-se-white" />
+        </label>
+        <!--  -->
+
+        <!-- Name -->
+        <label for="AcademicPo"
+               class="block overflow-hidden mb-8 rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600 bg-se-black1800">
+          <span class="text-xs font-medium text-se-white">
+            First name
+          </span>
+
+          <BaseInput v-model="student.firstname" type="text" placeholder="Insert Name here"
+                     class="mt-1 w-full border-none bg-transparent p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm text-se-white" />
+        </label>
+        <!--  -->
+
+        <!-- Last Name -->
+        <label for="AcademicPo"
+               class="block overflow-hidden mb-8 rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600 bg-se-black1800">
+          <span class="text-xs font-medium text-se-white">
+            Last name
+          </span>
+
+          <BaseInput v-model="student.surname" type="text" placeholder="Insert Last name here"
+                     class="mt-1 w-full border-none bg-transparent p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm text-se-white" />
+        </label>
+        <!--  -->
+
+        <!-- Department -->
+        <label for="AcademicPo"
+               class="block overflow-hidden mb-8 rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600 bg-se-black1800">
+          <span class="text-xs font-medium text-se-white">
+            Department
+          </span>
+
+          <BaseInput v-model="student.department" type="text" placeholder="Insert Department here"
+                     class="mt-1 w-full border-none bg-transparent p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm text-se-white" />
+        </label>
+
         <div>
-          <BaseInput v-model="student.studentId" type="text" label="Student ID"/>
-        </div>
-        <div>
-          <BaseInput v-model="student.firstname" type="text" label="First Name"/>
-        </div>
-        <div>
-          <BaseInput v-model="student.surname" type="text" label="Last Name"/>
-        </div>
-        <div>
-          <BaseInput v-model="student.department" type="text" label="Department"/>
-        </div>
-      
-        <div>
+          <label class="text-se-white">Advisor</label>
           <BaseSelect v-model="student.advisor.id"
-          :options="advisors"
-          :key-extractor="(x) => x.id"
-          :value-extractor="(x) => x.id"
-          :text-extractor="(x) => x.firstname+' '+x.surname"
-          label="Advisor"
-        />
+                      :options="advisors"
+                      :key-extractor="(x) => x.id"
+                      :value-extractor="(x) => x.id"
+                      :text-extractor="(x) => x.firstname+' '+x.surname"
+                      label=""
+                      class="mt-1 w-full border-none bg-transparent p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm text-se-white" />
         </div>
+
         <br>
         <div>
-          <label>Upload Profile Picture</label>
-          <ImageUpload v-model="student.images" class="px-4"/>
+          <h2 class="mb-5 text-sm text-se-white">
+            Upload Image here
+          </h2>
+          <ImageUpload v-model="student.images" />
           <br>
-          <button type="submit" class="px-4 py-1 pt-1 font-bold text-green-700 bg-transparent border border-green-700 rounded hover:bg-green-800 hover:text-white">Submit</button>
+
         </div>
+
+        <!-- button -->
+        <button class="group flex items-center justify-between gap-4 rounded-lg border border-current px-5 py-3 transition-colors hover:bg-se-color focus:outline-none focus:ring active:bg-se-color-light bg-se-black1800"
+                type="submit">
+          <span class="font-medium transition-colors text-se-white">
+            Submit
+          </span>
+
+          <span class="shrink-0 rounded-full border border-indigo-600 bg-se-black1800 p-2 group-active:border-indigo-500">
+            <svg class="h-5 w-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                 stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+            </svg>
+          </span>
+        </button>
       </div>
     </form>
 
